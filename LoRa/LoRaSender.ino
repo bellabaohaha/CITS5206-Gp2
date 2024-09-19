d0ee03a57c51c405a368a533865bb19bc082ea2d
baoh72821@gmail.com <baoh72821@gmail.com>
2024-09-19 19:56:46 +0800

    - remove unused code - refactored sensor values into integer array format

--- LoRa/LoRaSender.ino
+++ LoRa/LoRaSender.ino
@@ -11,68 +11,21 @@
 #ifndef CONFIG_RADIO_BW
 #define CONFIG_RADIO_BW             125.0
 #endif
- 
-TinyGPSPlus gps;
-HardwareSerial GPS(1);
-int counter = 0;
- 
+
 // data structure to send over LoRa
 struct SensorData
 {
-  int moisture_1;
-  int moisture_2;
-  int moisture_3;
-};
- 
- 
-struct BoardData
-{
-  int counter;
-  float latitude;
-  float longitude;
-  String date;
-  String time;
+  int moisture_sensor_values[3];   // integer array of size 3
 };
  
 // function to read sensor data and put into structure
-void readData(SensorData &sensorData, BoardData &boardData) {
+void readData(SensorData &sensorData) {
     // ====== READ actual moisture values from sensors ====== //
-    // Replace the following lines with actual sensor readings
- 
-    sensorData.moisture_1 = 123; // Example reading
-    sensorData.moisture_2 = 321; // Example reading
-    sensorData.moisture_3 = 456; // Example reading
- 
- 
-    // Board Data: GPS, Date
-    boardData.counter = counter;
- 
-    //  GPS
-    // if (gps.location.isValid()) {
-    //   boardData.latitude = gps.location.lat();
-    //   boardData.longitude = gps.location.lng();
-    // } else {
-    //   boardData.latitude = 0.0;
-    //   boardData.longitude = 0.0;
-    // }
- 
-//     // Date
-//     if (gps.date.isValid()) {
-//       boardData.date = String(gps.date.month()) + "/" + String(gps.date.day()) + "/" + String(gps.date.year());
-//     } else {
-//       boardData.date = "INVALID";
-//     }
- 
-//     // Time
-//     if (gps.time.isValid()) {
-//       boardData.time = (gps.time.hour() < 10 ? "0" : "") + String(gps.time.hour()) + ":"
-//                     + (gps.time.minute() < 10 ? "0" : "") + String(gps.time.minute()) + ":"
-//                     + (gps.time.second() < 10 ? "0" : "") + String(gps.time.second()) + "."
-//                     + (gps.time.centisecond() < 10 ? "0" : "") + String(gps.time.centisecond());
-//     } else {
-//       boardData.time = "INVALID";
-//     }
- 
+    // Replace with actual sensor readings in array format
+    sensorData.moisture_sensor_values[0] = 123; // Example reading
+    sensorData.moisture_sensor_values[1] = 321; // Example reading
+    sensorData.moisture_sensor_values[2] = 456; // Example reading
+
 }
  
 void setup()
@@ -80,8 +33,6 @@ void setup()
     setupBoards();
     // When the power is turned on, a delay is required.
     delay(1500);
-
-    GPS.begin(9600);  // 9600 is a common baud rate for GPS modules
     
     Serial.begin(115200);
  
@@ -117,28 +68,18 @@ void setup()
 void loop()
 {
   SensorData sensorData;
-  BoardData boardData;
+  String dataString;
  
+  // unpack sensor data and construct data string for sending over LoRa
+  for (int i = 0; i < 3; i++) {
+    if (i == 0) dataString += "["
+    dataString += String(sensorData.moisture_sensor_values[i]);
+    if (i == 2) dataString += "]";
+    if (i < 2) dataString += ",";
+  }
+
   readData(sensorData, boardData);
- 
-  // Construct the data string
-  String dataString = "";
-    // dataString += boardData.counter;
-    // dataString += ",";
-    // dataString += String(boardData.latitude, 6);  // 6 decimal places for latitude
-    // dataString += ",";
-    // dataString += String(boardData.longitude, 6); // 6 decimal places for longitude
-    // dataString += ",";
-    dataString += sensorData.moisture_1;
-    dataString += ",";
-    dataString += sensorData.moisture_2;
-    dataString += ",";
-    dataString += sensorData.moisture_3;
-    // dataString += ",";
-    // dataString += boardData.date; // Add the formatted date
-    // dataString += ",";
-    // dataString += boardData.time; // Add the formatted time
- 
+
   // Print the data string to the console
   Serial.print("Sending packet: ");
   Serial.println(dataString);
@@ -147,7 +88,6 @@ void loop()
   LoRa.beginPacket();
   LoRa.print(dataString);
   LoRa.endPacket();
- 
-  counter++;
+
   delay(2000);
 }
